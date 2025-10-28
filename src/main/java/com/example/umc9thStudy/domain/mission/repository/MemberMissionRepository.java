5d7fc62936b5dbfd2eeaa532f599526712890e00
alsrb0332@naver.com <alsrb0332@naver,com>
2025-10-28 21:27:14 +0900

    ✨ feat: 내가 진행중, 진행 완료한 미션 모아서 보는 쿼리

---
+++ src/main/java/com/example/umc9thStudy/domain/mission/repository/MemberMissionRepository.java
@@ -0,0 +1,67 @@
+package com.example.umc9thStudy.domain.mission.repository;
+
+import com.example.umc9thStudy.domain.mission.dto.MyMissionDto;
+import com.example.umc9thStudy.domain.mission.entity.MemberMission;
+import com.example.umc9thStudy.domain.mission.entity.Mission;
+import com.example.umc9thStudy.domain.mission.enums.Status;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+/*
+1주차 작성 쿼리
+
+#진행 중
+SELECT r.name, m.minimum_price, m.reward
+FROM mission AS m
+JOIN restaurant AS r ON r.restaurant_id = m.restaurant_id
+JOIN attempt AS a
+  ON a.mission_id = m.mission_id
+  AND a.user_id = 현재 사용자 아이디
+WHERE a.status = '진행중'
+AND m.created_at < ? --이전 요청 응답의 마지막 행
+ORDER BY m.mission_id DESC
+LIMIT ?;
+
+
+#진행 완료
+SELECT r.name, m.minimum_price, m.reward
+FROM mission AS m
+JOIN restaurant AS r ON r.restaurant_id = m.restaurant_id
+JOIN attempt AS a
+  ON a.mission_id = m.mission_id
+  AND a.user_id = 현재 사용자 아이디
+WHERE a.status = '완료'
+AND m.created_at < ? --이전 요청 응답의 마지막 행
+ORDER BY m.mission_id DESC
+LIMIT ?;
+ */
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+    @Query(
+            // DTO로 r.name, m.minimumPrice, m.reward 조회하기
+            "SELECT new com.example.umc9thStudy.domain.mission.dto.MyMissionDto(r.name, m.minimumPrice, m.rewardPoint)" +
+
+                    //JPQL로 조회
+                    "FROM MemberMission mm "+
+                    "JOIN mm.mission m " +
+                    "JOIN m.restaurant r "+
+
+                    "WHERE mm.member.id = :userId " + //mm이 가진 m에 접근 후 id를 받아옴
+                    "AND mm.status = :status " +
+
+                    //페이징
+                    "AND m.id < :lastMissionId " +
+
+                    "ORDER BY m.id DESC"
+    )
+    List<MyMissionDto> findMyMission(
+            @Param("userId") Long userId,
+            @Param("status") Status status,
+            @Param("lastMissionId") Long lastMissionId,
+            Pageable pageable
+    );
+
+    long countByMemberIdAndStatus(Long memberId, Status status);
+}
